de7b6be9bb5f27a0029d4f5715a15b1d61f89f51
Gizelle Fletcher <zelfletch@gmail.com>
2017-06-16 18:19:59 -0400

    added the ability to clear and reset the button

--- public/js/app.js
+++ public/js/app.js
@@ -32,8 +32,19 @@ const App = {
     cell.color = this.selectedColor;
     this.render();
   },
+  resetGrid: function(){
+    this.selectedColor = 'white';
+    this.makeGrid();
+    this.render();
+  },
   render:function(){
     this.root.innerHTML ='';
+    // this is the dynamic resetButton
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.addEventListener('click',() => this.resetGrid());
+    this.root.appendChild(resetButton);
+    // below is the grid part
     this.grid.forEach((row, rowIndex) =>{
       const rowContainer = document.createElement('div');
       rowContainer.style.height= `${this.cellHeight}px`;
