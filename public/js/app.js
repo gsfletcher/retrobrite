d1d679ac74489f18e193cd93293feae99fb1739c
Gizelle Fletcher <zelfletch@gmail.com>
2017-06-16 20:23:32 -0400

    added the ability to add a color based on a collection of three colorsthat define predetermined colors white blue and red

--- public/js/app.js
+++ public/js/app.js
@@ -24,9 +24,18 @@ const App = {
   },
   cacheDOM: function(){
     this.root = document.querySelector(this.rootElement);
-
+    this.colorButtons = this.root.querySelectorAll('button.color');
+    this.gridOutput = this.root.querySelector('.grid-output');
   },
-  bindEvents:function(){},
+  bindEvents:function(){
+    this.colorButtons.forEach(btn => {
+      const newColor = btn.classList[1];
+      btn.addEventListener('click',() => this.setColor(newColor));
+    });
+  },
+  setColor:function(newColor){
+    this.selectedColor = newColor;
+  },
   changeColor: function(rowIndex, colIndex){
     const cell = this.grid[rowIndex][colIndex];
     cell.color = this.selectedColor;
@@ -38,12 +47,12 @@ const App = {
     this.render();
   },
   render:function(){
-    this.root.innerHTML ='';
+    this.gridOutput.innerHTML ='';
     // this is the dynamic resetButton
     const resetButton = document.createElement('button');
     resetButton.textContent = 'Reset';
     resetButton.addEventListener('click',() => this.resetGrid());
-    this.root.appendChild(resetButton);
+    this.gridOutput.appendChild(resetButton);
     // below is the grid part
     this.grid.forEach((row, rowIndex) =>{
       const rowContainer = document.createElement('div');
@@ -54,7 +63,7 @@ const App = {
         element.addEventListener('click', () =>this.changeColor(rowIndex,colIndex));
         rowContainer.appendChild(element);
       });
-      this.root.appendChild(rowContainer);
+      this.gridOutput.appendChild(rowContainer);
     });
   },
 };
